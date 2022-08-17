ba81f9f0fa4bc2da2a15a864d4436d0e06b26598
yanka <56032650+yrribeiro@users.noreply.github.com>
2022-08-17 09:11:33 -0300

    Create expression-verifier.c

---
+++ expression-verifier.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(int argc, char *argv[]){
+    char* input = argv[1];
+    int i=0,len=strlen(input);
+    char term[2], oprts = {'+', '-', '*', '/'};
+    for(i=0; i<len; i++){
+        if (input[i] != '+')
+    }
+    return 0;
+}
